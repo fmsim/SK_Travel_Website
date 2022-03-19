bcfd64e995c54789ddc545769fa72af84e024136
Frank Simon <f.m.sim@t-online.de>
2022-03-19 22:45:23 +0100

    installed reac-router-dom, added Home.js, About.js and NotFound.js, modified App.js with BrowserRouter, Routes and Route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+import Home from "./pages/Home"
+import About from "./pages/About"
+import NotFound from "./pages/NotFound"
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello travel website</h1>
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
