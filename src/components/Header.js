496594f5fe921ccd432134eb8935581d378f5e34
Frank Simon <f.m.sim@t-online.de>
2022-03-26 16:45:23 +0100

    added poster and shape to video. added _variables.scss. added header content and styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,25 @@
 import { useState } from "react"
 
 const Header = () => {
-  const [state] = useState({ video: "/assets/videos/header.mp4" })
+  const [state] = useState({ video: "/assets/videos/header.mp4", poster: "/assets/images/screen.png" })
 
   return (
     <div className="header">
       <div className="header__video">
-        <video src={state.video} autoPlay loop muted></video>
+        <video src={state.video} autoPlay loop muted poster={state.poster}></video>
+      </div>
+      <div className="header__content">
+        <div className="container">
+          <div className="header__content__text">
+            <div className="header__content__text__child">
+              <h1 className="header__content__text__child__h1">We are travel friends</h1>
+              <p className="header__content__text__child__p">Come and join us we travel the most famous and beautiful places in the world</p>
+              <div className="header__content__text__child__link">
+                <button>Get Started</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
