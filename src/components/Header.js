f2b76647370dd910c8aea7354d2b10cf30ad0059
Frank Simon <f.m.sim@t-online.de>
2022-04-03 19:29:01 +0200

    added logo and styles. modified space inside content text

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,15 @@
 import { useState } from "react"
 
 const Header = ({ heading, paragraph, children }) => {
-  const [state] = useState({ video: "/assets/videos/header.mp4", poster: "/assets/images/screen.png" })
+  const [state] = useState({ video: "/assets/videos/header.mp4", poster: "/assets/images/screen.png", logo: "/assets/images/logo.png" })
 
   return (
     <div className="header">
+      <div className="container pr">
+        <div className="header__logo">
+          <img src={state.logo} alt="logo" />
+        </div>
+      </div>
       <div className="header__video">
         <video src={state.video} autoPlay loop muted poster={state.poster}></video>
       </div>
